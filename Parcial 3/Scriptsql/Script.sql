e5819ba0624f48d2a0d668b2e591d8dbe882f7aa
Victor M Rendon Local <garciamanuel0501@gmail.com>
2022-05-23 23:00:30 -0500

    sql

---
+++ Parcial 3/Scriptsql/Script.sql
@@ -0,0 +1,22 @@
+create database 19100238;
+use 19100238;
+
+create table empleado(
+idEmpleado smallint not null auto_increment,
+Nombres varchar(30),
+Apellidos varchar(50),
+CURP varchar(18),
+FechaNaci varchar(20),
+Genero varchar(15),
+Direccion varchar(100),
+Telefono varchar(10),
+Pais varchar(15),
+Estado varchar(15),
+Ciudad varchar(35),
+primary key (idEmpleado)
+);
+
+insert into empleado (Nombres, Apellidos, CURP, FechaNaci, Genero, 
+		 Direccion, Telefono, Pais, Estado, Ciudad)
+values ('Victor Manuel','Rendon Garcia','REGV010105HTSNRCA0', '05/01/2001', 'Masculino',
+             'Palmera #5642, Col. El Nogal', '8671922323', 'Mexico', 'Tamaulipas', 'Nuevo Laredo');
